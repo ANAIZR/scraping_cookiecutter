3c4d4d01acad7d2890ef19bf345d580d6e161bbc
Anaiz Rojas <ani.rojas.sen@gmail.com>
2024-12-10 11:48:39 -0500

    test of users

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,10 +1,12 @@
 from .base import *
 from .base import TEMPLATES
 from .base import env
+
 # GENERAL
+SECRET_KEY_TOKEN = "RHy4UEDieDE9H8L9W4WEKNz9vHUzM0sU7nk5x6pxa2dYxsqinP0Ml0xfOOgXlMfI"
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="RHy4UEDieDE9H8L9W4WEKNz9vHUzM0sU7nk5x6pxa2dYxsqinP0Ml0xfOOgXlMfI",
+    default=SECRET_KEY_TOKEN,
 )
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
@@ -18,4 +20,30 @@ EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 TEMPLATES[0]["OPTIONS"]["debug"] = True
 
 # MEDIA
-MEDIA_URL = "http://media.testserver"
+MEDIA_URL = "http://media.testserver/"
+
+DJANGO_APPS = [
+    "src.apps.core",
+    "src.apps.shared",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.forms",
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
+    "corsheaders",
+    "drf_spectacular",
+]
+
+LOCAL_APPS = [
+    "src.apps.users",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
