f24c069d03c99213bd9cb0e356f0731d60955682
Anaiz Rojas <ani.rojas.sen@gmail.com>
2024-12-13 16:18:44 -0500

    ajustando orígenes permitidos para CORS en la configuración del API

--- config/settings/base.py
+++ config/settings/base.py
@@ -199,12 +199,11 @@ SIMPLE_JWT = {
 
 CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://apiwebscraper.sgcan.dev/",
-    "https://webscraper.sgcan.dev/",
-    "0.0.0.0",
-    "localhost",
-    "127.0.0.1",
+    "http://0.0.0.0",
+    "http://127.0.0.1",
+    "http://localhost",
+    "https://apiwebscraper.sgcan.dev",
+    "https://webscraper.sgcan.dev",
 ]
 SPECTACULAR_SETTINGS = {
     "TITLE": "WEB SCRAPER API",
