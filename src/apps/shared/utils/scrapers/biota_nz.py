68619b27cd11702d76e282f9df9c333e1cd4e8e1
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-01-11 14:06:29 -0500

    actualizar scraper_biota_nz para mejorar la gestión de enlaces y tiempos de espera, y agregar nuevas palabras clave en plants.txt

--- src/apps/shared/utils/scrapers/biota_nz.py
+++ src/apps/shared/utils/scrapers/biota_nz.py
@@ -21,7 +21,7 @@ from rest_framework import status
 logger = get_logger("scraper")
 
 
-def load_keywords(file_path="../txt/all.txt"):
+def load_keywords(file_path="../txt/plants.txt"):
     try:
         base_path = os.path.dirname(os.path.abspath(__file__))
         absolute_path = os.path.join(base_path, file_path)
@@ -36,7 +36,7 @@ def load_keywords(file_path="../txt/all.txt"):
 
 def scraper_biota_nz(url, sobrenombre):
     driver = initialize_driver()
-
+    base_domain = "https://biotanz.landcareresearch.co.nz"
     try:
         driver.get(url)
         time.sleep(random.uniform(6, 10))
@@ -76,65 +76,88 @@ def scraper_biota_nz(url, sobrenombre):
                 continue
             while True:
                 try:
-                    WebDriverWait(driver, 10).until(
+                    WebDriverWait(driver, 30).until(
                         EC.presence_of_element_located((By.ID, "list-result"))
                     )
                     logger.info(f"Resultados cargados para: {keyword}")
                     soup = BeautifulSoup(driver.page_source, "html.parser")
-                    items = soup.select("div.row-separation div.col-12.a")
+                    items = soup.select("div.row-separation.specimen-list-item")
 
                     print(f"Encontrados {len(items)} resultados.")
 
                     for item in items:
-                        href = item.find("a")["href"]
-                        print(f"Enlace encontrado: {href}")
-                        if href:
-                            logger.info(f"Accediendo al enlace: {href}")
+                        try:
+                            link_element = item.select_one("div.col-12 > a")
+                            if link_element:
+                                href_element = item.select_one("div.col-12 > a[href]")
+                                href = href_element["href"]
+                                print(f"Enlace encontrado: {href}")
+                                full_url = f"{base_domain}{href}"
 
-                            driver.get(href)
-                            visited_urls.add(href)
-                            WebDriverWait(driver, 10).until(
-                                EC.presence_of_element_located(
-                                    (By.CSS_SELECTOR, "div.page-content-wrapper")
-                                )  
-                            )
-                            time.sleep(random.uniform(6, 10))
-                            soup = BeautifulSoup(driver.page_source, "html.parser")
-                            body = soup.select_one("div.page-content-wrapper")
-                            body_text = (
-                                body.get_text(strip=True) if body else "No body found"
-                            )
-                            if body_text:
-                                contenido = f"{body_text}\n\n\n"
-                                link_folder = generate_directory(keyword_folder, href)
-                                file_path = get_next_versioned_filename(
-                                    link_folder, base_name=sobrenombre
-                                )
-                                with open(file_path, "w", encoding="utf-8") as file:
-                                    file.write(contenido)
-
-                                with open(file_path, "rb") as file_data:
-                                    object_id = fs.put(
-                                        file_data, filename=os.path.basename(file_path)
+                                driver.get(full_url)
+                                visited_urls.add(full_url)
+                                WebDriverWait(driver, 10).until(
+                                    EC.presence_of_element_located(
+                                        (By.CSS_SELECTOR, "div.page-content-wrapper")
                                     )
+                                )
+                                time.sleep(random.uniform(6, 10))
+                                soup = BeautifulSoup(driver.page_source, "html.parser")
+                                body = soup.select_one("div.page-content-wrapper")
+                                body_text = (
+                                    body.get_text(strip=True)
+                                    if body
+                                    else "No body found"
+                                )
+                                if body_text:
+                                    contenido = f"{body_text}\n\n\n"
+                                    link_folder = generate_directory(
+                                        href, keyword_folder
+                                    )
+                                    file_path = get_next_versioned_filename(
+                                        link_folder, base_name=sobrenombre
+                                    )
+                                    with open(file_path, "w", encoding="utf-8") as file:
+                                        file.write(contenido)
 
-                                print(f"Página procesada y guardada: {href}")
-                            else:
-                                print("No se encontró contenido en la página.")
-                            driver.back()
-                            WebDriverWait(driver, 30).until(
-                                EC.presence_of_element_located((By.ID, "list-result"))
-                            )
-                            time.sleep(random.uniform(3, 6))
+                                    with open(file_path, "rb") as file_data:
+                                        object_id = fs.put(
+                                            file_data,
+                                            filename=os.path.basename(file_path),
+                                        )
+
+                                    print(f"Página procesada y guardada: {href}")
+                                else:
+                                    print("No se encontró contenido en la página.")
+                                driver.back()
+                                WebDriverWait(driver, 30).until(
+                                    EC.presence_of_element_located(
+                                        (By.ID, "list-result")
+                                    )
+                                )
+                                time.sleep(random.uniform(3, 6))
+
+                        except Exception:
+                            logger.error("No tiene url")
+                            continue
+                    print("Fin de la página.")
                     try:
-                        next_page = driver.find_element_by_css_selector(
-                            "a.next.page-numbers"
+                        next_page = WebDriverWait(driver, 10).until(
+                            EC.element_to_be_clickable(
+                                (
+                                    By.XPATH,
+                                    "//a[contains(@class, 'paging-hyperlink') and contains(text(), 'Next')]",
+                                )
+                            )
                         )
-                        next_page.click()
+                        driver.execute_script("arguments[0].click();", next_page)
                         time.sleep(random.uniform(6, 10))
                     except Exception as e:
-                        logger.info("No hay más páginas disponibles.")
-                        break
+                        logger.info(
+                            "No hay más páginas disponibles o el botón no es clickeable."
+                        )
+                        driver.get(url)
+                        continue
                 except Exception as e:
                     print(f"Error al procesar resultados: {e}")
                     scraping_failed = True
@@ -149,7 +172,6 @@ def scraper_biota_nz(url, sobrenombre):
             )
         else:
             data = {
-                "Objeto": object_id,
                 "Tipo": "Web",
                 "Url": url,
                 "Fecha_scrapper": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
