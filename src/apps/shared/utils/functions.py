81abd02884bddb29aa7c40e2094490a6fde0c7c9
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-01-12 17:49:16 -0500

    eliminar la opción de ejecución en modo headless en la inicialización del controlador

--- src/apps/shared/utils/functions.py
+++ src/apps/shared/utils/functions.py
@@ -58,7 +58,7 @@ def initialize_driver():
         options.binary_location = "/usr/bin/google-chrome"
         # options.binary_location = "/opt/google/chrome/google-chrome"
 
-        options.add_argument("--headless")
+        #options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
