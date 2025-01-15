3b44c6803937808557d6c58a29fbbb8fb87b86c1
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-01-15 17:34:48 -0500

    Agregar almacenamiento de access token en el modelo de usuario y mejorar la gestión de fechas en ScraperURL

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -25,8 +25,12 @@ class User(BaseUserModel):
     email = models.EmailField(unique=True)
     system_role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=2)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(blank=True, null=True, db_index=True, editable=False)
+    deleted_at = models.DateTimeField(
+        blank=True, null=True, db_index=True, editable=False
+    )
     is_active = models.BooleanField(default=True)
+    access_token = models.CharField(max_length=500, blank=True, null=True)
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "password", "is_active"]
 
