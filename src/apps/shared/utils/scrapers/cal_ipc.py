69d76f1fa6d6ef15d7153bcf10054a496822c1eb
ANAIZR <ani.rojas.sen@gmail.com>
2025-01-23 11:48:27 -0500

    Update CAL-IPC scraper: modify URL, enhance logging, and streamline link extraction

--- src/apps/shared/utils/scrapers/cal_ipc.py
+++ src/apps/shared/utils/scrapers/cal_ipc.py
@@ -9,7 +9,7 @@ from ..functions import (
     get_logger,
     get_random_user_agent,
 )
-
+import time
 
 def scraper_cal_ipc(url, sobrenombre, max_depth=3):
     headers = {"User-Agent": get_random_user_agent()}
@@ -30,73 +30,29 @@ def scraper_cal_ipc(url, sobrenombre, max_depth=3):
         if current_url in visited_urls or current_depth > max_depth:
             return []
 
-        if not current_url.startswith(base_domain):
-            print(f"URL fuera del dominio permitido: {current_url}")
-            return []
-        if any(
-            exclude in current_url
-            for exclude in [
-                "solutions",
-                "support",
-                "join",
-                "about",
-                "discrimination",
-                "resources",
-                "mailto",
-                "pdf"
-            ]
-        ):
-            print(f"URL filtrada por parámetros no válidos: {current_url}")
-            return []
+        
 
-        print(f"Procesando URL: {current_url} (Nivel: {current_depth})")
+        logger.info(f"Procesando URL: {current_url} (Nivel: {current_depth})")
         visited_urls.add(current_url)
 
         try:
-            response = requests.get(current_url, headers=headers)
+            response = requests.get(current_url, headers=headers,timeout=20)
             response.raise_for_status()
-
+            time.sleep(1)
             soup = BeautifulSoup(response.text, "html.parser")
 
-            links = soup.find_all("a", href=True)
-            base_url = base_domain
+            table = soup.find("table", id="paftable")
+            links = table.find_all("a", href=True) if table else []
             extracted_links = []
 
             for link in links:
                 href = link["href"]
-                if href == "#" or not href or href.startswith("#"):
-                    continue
-                if href.startswith("/"):
-                    href = urljoin(base_url, href)
 
-                if not href.startswith(base_domain):
-                    continue
-                if any(
-                    exclude in href
-                    for exclude in [
-                        "solutions",
-                        "support",
-                        "join",
-                        "about",
-                        "discrimination",
-                        "resources",
-                        "mailto",
-                        "pdf"
-                    ]
+                if href.startswith(base_domain) and not any(
+                    href.endswith(ext) for ext in [".jpg", ".jpeg", ".png", ".gif"]
                 ):
-                    continue
-                if (
-                    href.endswith(".pdf")
-                    or href.endswith(".jpg")
-                    or href.endswith(".jpeg")
-                    or href.endswith(".png")
-                    or href.endswith(".gif")
-                    or "images.bugwood.org" in href
-                ):
-                    continue
-
-                print(f"URL válida encontrada ---> : {href}")
-                extracted_links.append((href, current_depth + 1))
+                    logger.info(f"URL válida encontrada: {href}")
+                    extracted_links.append((href, current_depth + 1))
 
             total_urls_found += len(extracted_links)
 
@@ -111,7 +67,7 @@ def scraper_cal_ipc(url, sobrenombre, max_depth=3):
             return extracted_links
 
         except requests.RequestException as e:
-            print(f"Error al procesar la URL: {current_url}, error: {str(e)}")
+            logger.error(f"Error al procesar la URL: {current_url}, error: {str(e)}")
             urls_not_scraped.append(current_url)
             return []
 
@@ -126,7 +82,7 @@ def scraper_cal_ipc(url, sobrenombre, max_depth=3):
                     new_links = future.result()
                     urls_to_scrape.extend(new_links)
                 except Exception as e:
-                    print(f"Error en tarea de scraping: {str(e)}")
+                    logger.error(f"Error en tarea de scraping: {str(e)}")
 
     try:
         urls_to_scrape.append((url, 0))
@@ -153,6 +109,7 @@ def scraper_cal_ipc(url, sobrenombre, max_depth=3):
 
         response = process_scraper_data(all_scraper, url, sobrenombre, collection, fs)
         return response
+
     except requests.RequestException as e:
         return JsonResponse(
             {"error": f"Error al realizar la solicitud: {str(e)}"}, status=400
