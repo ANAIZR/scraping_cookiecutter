eaa89501a09a3314693bea4a47c320d6d4038556
ANAIZR <ani.rojas.sen@gmail.com>
2025-02-03 15:52:08 -0500

    add logging to coleoptera_neotropical scraper for better traceability

--- src/apps/shared/utils/scrapers/coleoptera_neotropical.py
+++ src/apps/shared/utils/scrapers/coleoptera_neotropical.py
@@ -7,17 +7,15 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from ..functions import (
-    generate_directory,
-    get_next_versioned_filename,
-    delete_old_documents,
     connect_to_mongo,
     initialize_driver,
-    save_scraper_data
+    save_scraper_data,
+    get_logger
 )
 
-def scraper_coleoptera_neotropical(url, sobrenombre):
+def scraper_coleoptera_neotropical(url, sobrenombre):    
+    logger = get_logger("Inciando scraper")
     driver = initialize_driver()
-
     collection, fs = connect_to_mongo()
 
     try:
@@ -42,7 +40,6 @@ def scraper_coleoptera_neotropical(url, sobrenombre):
             scrape_count += 1  
 
         all_scraper = f"{all_scraper}Filas scrapeadas: {scrape_count}\n"
-
         response_data = save_scraper_data(all_scraper,url,sobrenombre,collection,fs)
         return response_data
 
