b5eb5e1ce1105b7ac0a44b7e792ae902efa62e6c
ANAIZR <ani.rojas.sen@gmail.com>
2025-02-17 12:07:57 -0500

    Refactor flora_harvard scraper: improve error handling, enhance data storage with version control, and optimize PDF processing

--- src/apps/shared/utils/scrapers/flora_harvard.py
+++ src/apps/shared/utils/scrapers/flora_harvard.py
@@ -1,51 +1,49 @@
-import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from django.http import JsonResponse
+from datetime import datetime
+from bson import ObjectId
 from ..functions import (
-    process_scraper_data,
+    process_scraper_data_v2,
     connect_to_mongo,
     get_logger,
     get_random_user_agent,
+    extract_text_from_pdf
 )
 from rest_framework.response import Response
 from rest_framework import status
 
 def scraper_flora_harvard(url, sobrenombre):
-    """
-    Scraper que extrae enlaces dentro de <ul>, descarga PDFs y extrae texto de 'panelTaxonTreatment'.
-    """
+
     headers = {"User-Agent": get_random_user_agent()}
     logger = get_logger("scraper")
     collection, fs = connect_to_mongo("scrapping-can", "collection")
-    all_scraper = ""
+
+    all_scraper = "" 
     visited_urls = set()
     urls_not_scraped = []
-    pdf_folder = "pdfs"  # Carpeta única para todos los PDFs
-
-    # Crear carpeta para PDFs
-    os.makedirs(pdf_folder, exist_ok=True)
+    scraped_urls = []
+    total_scraped_links = 0
 
     def get_page_content(current_url):
-        """
-        Realiza una solicitud GET y devuelve el contenido HTML.
-        """
-        response = requests.get(current_url, headers=headers)
-        response.raise_for_status()
-        return response.text
+
+        try:
+            response = requests.get(current_url, headers=headers)
+            response.raise_for_status()
+            return response.text
+        except requests.RequestException as e:
+            logger.error(f"Error al obtener el contenido de {current_url}: {e}")
+            urls_not_scraped.append(current_url)
+            return None
 
     def extract_hrefs_from_ul(html_content, base_url):
-        """
-        Extrae todos los enlaces de las etiquetas <ul>.
-        """
+ 
         soup = BeautifulSoup(html_content, "html.parser")
         hrefs = []
         for ul in soup.find_all("ul"):
             links = ul.find_all("a", href=True)
             for link in links:
                 href = link["href"]
-                # Convertir enlaces relativos a absolutos
                 if href.startswith("/"):
                     href = urljoin(base_url, href)
                 elif href.endswith(".pdf"):
@@ -53,29 +51,9 @@ def scraper_flora_harvard(url, sobrenombre):
                 hrefs.append(href)
         return hrefs
 
-    def download_pdf(pdf_url):
-        """
-        Descarga un archivo PDF en la carpeta única correspondiente.
-        """
-        try:
-            response = requests.get(pdf_url, headers=headers, stream=True)
-            response.raise_for_status()
-
-            pdf_name = os.path.basename(pdf_url)
-            pdf_path = os.path.join(pdf_folder, pdf_name)
-
-            with open(pdf_path, "wb") as pdf_file:
-                for chunk in response.iter_content(chunk_size=1024):
-                    pdf_file.write(chunk)
-
-            logger.info(f"PDF descargado: {pdf_path}")
-        except Exception as e:
-            logger.error(f"Error al descargar el PDF {pdf_url}: {str(e)}")
-            urls_not_scraped.append(pdf_url)
-
     def process_link(current_url):
-        
-        nonlocal all_scraper
+ 
+        nonlocal total_scraped_links, all_scraper
 
         if current_url in visited_urls:
             return
@@ -85,17 +63,93 @@ def scraper_flora_harvard(url, sobrenombre):
 
         try:
             html_content = get_page_content(current_url)
+            if not html_content:
+                return
+
             soup = BeautifulSoup(html_content, "html.parser")
 
             panel_treatment = soup.find("div", id="panelTaxonTreatment")
             if panel_treatment:
-                content = panel_treatment.get_text(strip=True)
-                all_scraper += f"URL: {current_url}\n\n{content}\n{'-' * 80}\n\n"
+                content_text = panel_treatment.get_text(strip=True)
+
+                object_id = fs.put(
+                    content_text.encode("utf-8"),
+                    source_url=current_url,
+                    scraping_date=datetime.now(),
+                    Etiquetas=["planta", "plaga"],
+                    contenido=content_text,
+                    url=url  # URL principal
+                )
+
+                collection.insert_one(
+                    {
+                        "_id": object_id,
+                        "source_url": current_url,
+                        "scraping_date": datetime.now(),
+                        "Etiquetas": ["planta", "plaga"],
+                        "contenido": content_text,
+                        "url": url,
+                    }
+                )
+
+                scraped_urls.append(current_url)
+                total_scraped_links += 1
+
+                existing_versions = list(
+                    collection.find({"source_url": current_url}).sort("scraping_date", -1)
+                )
+
+                if len(existing_versions) > 1:
+                    oldest_version = existing_versions[-1]
+                    fs.delete(ObjectId(oldest_version["_id"]))
+                    collection.delete_one({"_id": ObjectId(oldest_version["_id"])})
+
+                    logger.info(
+                        f"Se eliminó la versión más antigua con este enlace: '{current_url}' y object_id: {oldest_version['_id']}"
+                    )
 
             links = extract_hrefs_from_ul(html_content, url)
+
             for link in links:
                 if link.endswith(".pdf"):
-                    download_pdf(link)
+                    pdf_content = extract_text_from_pdf(link)
+
+                    if pdf_content:
+                        object_id = fs.put(
+                            pdf_content.encode("utf-8"),
+                            source_url=link,
+                            scraping_date=datetime.now(),
+                            Etiquetas=["planta", "plaga"],
+                            contenido=pdf_content,
+                            url=url
+                        )
+
+                        collection.insert_one(
+                            {
+                                "_id": object_id,
+                                "source_url": link,
+                                "scraping_date": datetime.now(),
+                                "Etiquetas": ["planta", "plaga"],
+                                "contenido": pdf_content,
+                                "url": url,
+                            }
+                        )
+
+                        scraped_urls.append(link)
+                        total_scraped_links += 1
+
+                        existing_versions = list(
+                            collection.find({"source_url": link}).sort("scraping_date", -1)
+                        )
+
+                        if len(existing_versions) > 1:
+                            oldest_version = existing_versions[-1]
+                            fs.delete(ObjectId(oldest_version["_id"]))
+                            collection.delete_one({"_id": ObjectId(oldest_version["_id"])})
+
+                            logger.info(
+                                f"Se eliminó la versión más antigua con este enlace: '{link}' y object_id: {oldest_version['_id']}"
+                            )
 
         except Exception as e:
             logger.error(f"Error al procesar la URL {current_url}: {str(e)}")
@@ -103,28 +157,32 @@ def scraper_flora_harvard(url, sobrenombre):
 
     try:
         main_html = get_page_content(url)
+        if not main_html:
+            return Response({"error": "No se pudo acceder a la página principal"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         links = extract_hrefs_from_ul(main_html, url)
         logger.info(f"Total de enlaces encontrados: {len(links)}")
 
-        # Procesar cada enlace
         for link in links:
             process_link(link)
 
         all_scraper = (
             f"Resumen del scraping:\n"
-            f"Total de URLs procesadas: {len(visited_urls)}\n"
-            f"Total de PDFs descargados: {len(os.listdir(pdf_folder))}\n"
-            f"Total de URLs no procesadas: {len(urls_not_scraped)}\n\n"
+            f"Total de URLs procesadas: {len(scraped_urls)}\n"
+            f"Total de URLs no procesadas: {len(urls_not_scraped)}\n"
+            f"Total de archivos almacenados en MongoDB: {total_scraped_links}\n\n"
             f"{'-'*80}\n\n"
-        ) + all_scraper
+        )
+
+        if scraped_urls:
+            all_scraper += "URLs scrapeadas:\n" + "\n".join(scraped_urls) + "\n\n"
 
         if urls_not_scraped:
-            all_scraper += "URLs no procesadas:\n\n" + "\n".join(urls_not_scraped) + "\n"
+            all_scraper += "URLs no procesadas:\n" + "\n".join(urls_not_scraped) + "\n"
 
-        response = process_scraper_data(all_scraper, url, sobrenombre, collection, fs)
+        response = process_scraper_data_v2(all_scraper, url, sobrenombre)
         return response
 
     except Exception as e:
         logger.error(f"Error general en el proceso de scraping: {str(e)}")
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
