ce6dbab0614f9d8ec0a1675cb7979d448fc7147f
ANAIZR <ani.rojas.sen@gmail.com>
2025-02-19 15:39:52 -0500

    fix: adjust version deletion logic and clean up whitespace in scrapers

--- src/apps/shared/utils/scrapers/agriculture_gov.py
+++ src/apps/shared/utils/scrapers/agriculture_gov.py
@@ -131,7 +131,7 @@ def scraper_agriculture_gov(url, sobrenombre):
                                 fs.find({"source_url": href}).sort("scraping_date", -1)
                             )
 
-                            if len(existing_versions) > 2:
+                            if len(existing_versions) > 1:
                                 oldest_version = existing_versions[-1]
                                 fs.delete(ObjectId(oldest_version["_id"]))
                                 logger.info(f"Se eliminó la versión más antigua con este enlace: '{href}' y object_id: {oldest_version['_id']}")
