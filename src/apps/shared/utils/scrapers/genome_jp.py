74f04a2012e3e8f8b64a8dbe93ef6364cd5ef46c
ANAIZR <ani.rojas.sen@gmail.com>
2025-02-21 19:49:50 -0500

    feat: improve genome_jp scraper to enhance error logging and data extraction process

--- src/apps/shared/utils/scrapers/genome_jp.py
+++ src/apps/shared/utils/scrapers/genome_jp.py
@@ -1,5 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import time
 from datetime import datetime
+from bson import ObjectId
 from ..functions import (
     process_scraper_data,
     connect_to_mongo,
@@ -22,16 +21,15 @@ def navigate_to_next_page(driver, wait_time):
         next_button.click()
         time.sleep(2)
         return True
-    except Exception as e:
-        print(f"No se pudo navegar a la siguiente página: {e}")
-        return False
+    except Exception:
+        return False 
 
 def extract_data(driver, wait_time, fs, logger, url):
     all_scraper = ""
     record_count = 1
     skipped_rows = 0
     total_scraped_successfully = 0
-    
+
     try:
         tbody = WebDriverWait(driver, wait_time).until(
             EC.presence_of_element_located((By.XPATH, "//table/tbody[2]"))
@@ -50,7 +48,6 @@ def extract_data(driver, wait_time, fs, logger, url):
             host_name = cols[2].text.strip()
             tipos = cols[3].text.strip()
             
-            # Extraer enlaces dentro de la fila
             link_elements = row.find_elements(By.TAG_NAME, "a")
             for link in link_elements:
                 link_href = link.get_attribute("href")
@@ -58,7 +55,7 @@ def extract_data(driver, wait_time, fs, logger, url):
                     driver.get(link_href)
                     time.sleep(2)
                     content_text = driver.find_element(By.TAG_NAME, "body").text
-                    
+
                     if content_text:
                         object_id = fs.put(
                             content_text.encode("utf-8"),
@@ -70,19 +67,21 @@ def extract_data(driver, wait_time, fs, logger, url):
                         )
                         total_scraped_successfully += 1
                         logger.info(f"Archivo almacenado en MongoDB con object_id: {object_id}")
-                        
+
                         existing_versions = list(fs.find({"source_url": link_href}).sort("scraping_date", -1))
                         if len(existing_versions) > 1:
                             oldest_version = existing_versions[-1]
                             fs.delete(ObjectId(oldest_version["_id"]))
                             logger.info(f"Se eliminó la versión más antigua con object_id: {oldest_version['_id']}")
 
-            all_scraper += f"Registro #{record_count} : \n"
-            all_scraper += f"Virus(species) name :      {titulo}\n"
-            all_scraper += f"Virus lineage       :      {descripcion}\n"
-            all_scraper += f"Host name           :      {host_name}\n"
-            all_scraper += f"Host lineage        :      {tipos}\n"
-            all_scraper += "-------------------------------------------\n\n"
+            all_scraper += (
+                f"Registro #{record_count} : \n"
+                f"Virus(species) name :      {titulo}\n"
+                f"Virus lineage       :      {descripcion}\n"
+                f"Host name           :      {host_name}\n"
+                f"Host lineage        :      {tipos}\n"
+                "-------------------------------------------\n\n"
+            )
 
             record_count += 1
 
@@ -91,7 +90,7 @@ def extract_data(driver, wait_time, fs, logger, url):
 
         return all_scraper, total_scraped_successfully
     except Exception as e:
-        print(f"Error al extraer datos: {e}")
+        logger.error(f"Error al extraer datos: {e}")
         return all_scraper, total_scraped_successfully
 
 def scraper_genome_jp(url, sobrenombre):
@@ -100,6 +99,7 @@ def scraper_genome_jp(url, sobrenombre):
     driver = initialize_driver()
     collection, fs = connect_to_mongo()
     all_scraper = ""
+
     try:
         driver.get(url)
 
@@ -110,12 +110,16 @@ def scraper_genome_jp(url, sobrenombre):
             if not navigate_to_next_page(driver, 30):
                 break
 
-        response = process_scraper_data(all_scraper, url, sobrenombre, collection, fs)
+        if not all_scraper:
+            raise Exception("No se pudo extraer ningún dato.")
+
         logger.info(f"Total de enlaces visitados y almacenados: {total_scraped_successfully}")
+
+        response = process_scraper_data(all_scraper, url, sobrenombre)
         return response
 
     except Exception as e:
-        print(f"Error general en el scraping: {e}")
+        logger.error(f"Error general en el scraping: {e}")
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     finally:
