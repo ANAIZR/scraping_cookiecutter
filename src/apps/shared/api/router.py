9b6ef39228349998ff812dc9020fefab134e8757
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-02-22 20:15:29 -0500

    feat: agregar nuevas rutas para la gestión de suscripciones y comparación de reportes en el API

--- src/apps/shared/api/router.py
+++ src/apps/shared/api/router.py
@@ -4,12 +4,32 @@ from django.urls import path
 from .viewsets.login_viewsets import LoginView
 from .viewsets import get_viewset_scraper, scraperURL_viewsets
 from .viewsets.scraperFixture_viewsets import ScraperAPIView
+from .viewsets.scraperURL_viewsets import (
+    SpeciesViewSet,
+    ReportComparisonDetailView,
+    SpeciesSubscriptionViewSet,
+)
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 router.register(r"urls", scraperURL_viewsets.ScraperURLViewSet, basename="urls")
 router.register(r"species", scraperURL_viewsets.SpeciesURLViewSet, basename="especies")
+router.register(
+    r"subscription", SpeciesSubscriptionViewSet, basename="subscription"
+)
+
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path("scraper-get-url/", get_viewset_scraper.get_scraper_url, name="get_scraper_url"),
-    path("scraper-url/",  ScraperAPIView.as_view(), name="scraper_url"),
+    path(
+        "scraper-get-url/", get_viewset_scraper.get_scraper_url, name="get_scraper_url"
+    ),
+    path("scraper-url/", ScraperAPIView.as_view(), name="scraper_url"),
+    path(
+        "report-comparison/<int:scraper_id>/",
+        ReportComparisonDetailView.as_view(),
+        name="report-comparison-detail",
+    ),
+
 ]
 urlpatterns += router.urls
+
+
+
