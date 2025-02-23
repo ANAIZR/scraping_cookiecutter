9c1e32f4679b8d7c03e279e374da8aa1ac037381
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-02-23 14:48:53 -0500

    fix: actualizar la programación de la tarea de scraping de URLs expiradas

--- celery_app.py
+++ celery_app.py
@@ -19,7 +19,7 @@ def debug_task(self):
 app.conf.beat_schedule = {
     "scrape-expired-urls-weekly": {
         "task": "src.apps.shared.utils.tasks.scraper_expired_urls_task",  
-        "schedule": crontab(hour=1, minute=55, day_of_week=5),  
+        "schedule": crontab(hour=22, minute=0, day_of_week=0),  
     }
 }
 
