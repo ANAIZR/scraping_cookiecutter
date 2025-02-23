819d5f76d368e5ce246fb5d4073f62f7bccfe4b4
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-02-22 20:07:58 -0500

    feat: agregar nuevos serializadores para ReportComparison y SpeciesSubscription, y campos adicionales en ScraperURL y Species

--- src/apps/shared/api/serializers/scraperURL_serializers.py
+++ src/apps/shared/api/serializers/scraperURL_serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
-from ...models.scraperURL import ScraperURL, Species
+from src.apps.shared.models.scraperURL import ScraperURL, Species, ReportComparison, SpeciesSubscription
 
 
 class ScraperURLSerializer(serializers.ModelSerializer):
     time_choices_display = serializers.CharField(
         source="get_time_choices_display", read_only=True
     )
+    estado_scrapeo = serializers.CharField(read_only=True)
+    error_scrapeo = serializers.CharField(read_only=True)
 
     class Meta:
         model = ScraperURL
@@ -16,6 +18,8 @@ class ScraperURLSerializer(serializers.ModelSerializer):
             "time_choices",
             "fecha_scraper", 
             "time_choices_display",
+            "estado_scrapeo",  
+            "error_scrapeo",    
             "created_at",
             "updated_at",
         )
@@ -37,8 +41,30 @@ class ScraperURLSerializer(serializers.ModelSerializer):
         return value
 
 
-
 class SpeciesSerializer(serializers.ModelSerializer):
+    sobrenombre = serializers.CharField(source='scraper_source.sobrenombre', read_only=True)  
+
     class Meta:
         model = Species
-        fields = "__all__"
+        fields = '__all__' 
+        extra_fields = ['sobrenombre']
+
+
+class ReportComparisonSerializer(serializers.ModelSerializer):
+    url = serializers.CharField(source='scraper_source.url', read_only=True)
+    scraper_source = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = ReportComparison
+        fields = '__all__'
+
+class SpeciesSubscriptionSerializer(serializers.ModelSerializer):
+    scientific_name = serializers.CharField(required=False)
+    distribution = serializers.CharField(required=False)
+    hosts = serializers.CharField(required=False)
+    name_subscription = serializers.CharField(required=True)
+    
+    class Meta:
+        model = SpeciesSubscription
+        fields = ["id", "user", "scientific_name", "distribution", "hosts","name_subscription", "created_at"]
+        read_only_fields = ["id", "user", "created_at"]
