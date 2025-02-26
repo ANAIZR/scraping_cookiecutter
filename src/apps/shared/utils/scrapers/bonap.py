23e34127a0acf3b45d4db19d3ebda8caf57d858e
qumadev <adrian.mamani.dev@gmail.com>
2025-02-26 16:42:33 -0500

    url_scraper_bonap_MONGO

--- src/apps/shared/utils/scrapers/bonap.py
+++ src/apps/shared/utils/scrapers/bonap.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from ..functions import (
-    process_scraper_data,
+    process_scraper_data_v2,
     connect_to_mongo,
     get_logger,
     initialize_driver,
@@ -9,7 +9,8 @@ from ..functions import (
 from rest_framework.response import Response
 from rest_framework import status
 import time
-
+from datetime import datetime
+from bson import ObjectId
 
 
 def scraper_bonap(
@@ -23,6 +24,10 @@ def scraper_bonap(
     collection, fs = connect_to_mongo("scrapping-can", "collection")
 
     all_scraper = ""
+    non_scraped_urls = []  
+    scraped_urls = []
+    total_scraped_links = 0
+    index = 0
 
     try:
         driver.get(url)
@@ -44,6 +49,9 @@ def scraper_bonap(
 
                 species_list = driver.find_elements(By.CSS_SELECTOR, "#species-list li")
                 for species in species_list:
+                    index += 1
+                    href = url + f"-{index}"
+                    print("href by quma: ", href)
                     species_name = species.text.strip()
 
                     species.click()
@@ -52,13 +60,43 @@ def scraper_bonap(
                     try:
                         content_div = driver.find_element(By.ID, "view-frame")
                         content = content_div.text.strip()
+                        body_content = f"1 : # {family_name} - {genus_name} - {species_name}\nContenido :\n{content}"
+                        if body_content:
+                            object_id = fs.put(
+                                body_content.encode("utf-8"),
+                                source_url=href,
+                                scraping_date=datetime.now(),
+                                Etiquetas=["planta", "plaga"],
+                                contenido=body_content,
+                                url=url
+                            )
+                            total_scraped_links += 1
+                            scraped_urls.append(href)
+                            logger.info(f"Archivo almacenado en MongoDB con object_id: {object_id}")
 
-                        all_scraper += f"1 : # {family_name} - {genus_name} - {species_name}\nContenido :\n{content}"
+                            existing_versions = list(fs.find({"source_url": href}).sort("scraping_date", -1))
+                            if len(existing_versions) > 1:
+                                oldest_version = existing_versions[-1]
+                                file_id = oldest_version._id  # Esto obtiene el ID correcto
+                                fs.delete(file_id)  # Eliminar la versión más antigua
+                                logger.info(f"Se eliminó la versión más antigua con object_id: {file_id}")
+                        else:
+                            non_scraped_urls.append(href)                        
 
                     except Exception as e:
                         print(f"    Error al extraer contenido: {e}")
-        response = process_scraper_data(all_scraper, url, sobrenombre, collection, fs)
+
+
+        all_scraper = (
+            f"Total enlaces scrapeados: {len(scraped_urls)}\n"
+            f"URLs scrapeadas:\n" + "\n".join(scraped_urls) + "\n\n"
+            f"Total enlaces no scrapeados: {len(non_scraped_urls)}\n"
+            f"URLs no scrapeadas:\n" + "\n".join(non_scraped_urls) + "\n"
+        )
+
+        response = process_scraper_data_v2(all_scraper, url, sobrenombre)
         return response
+
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
