b2a8c42c8b73962cd0a04ac21fc8dcf7d00aec26
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2025-02-25 20:32:45 -0500

    Enhance nemaplex scraper: improve error handling, track total links and scrape results, and update data processing function

--- src/apps/shared/utils/scrapers/nemaplex_plant_host.py
+++ src/apps/shared/utils/scrapers/nemaplex_plant_host.py
@@ -6,65 +6,93 @@ from rest_framework.response import Response
 from rest_framework import status
 from bs4 import BeautifulSoup
 from ..functions import (
-    process_scraper_data,
+    process_scraper_data_v2,
     connect_to_mongo,
     get_logger,
     initialize_driver,
 )
+import time
+import random
 
-def scraper_nemaplex_plant_host(url,sobrenombre):
+def scraper_nemaplex_plant_host(url, sobrenombre):
     logger = get_logger("scraper")
     logger.info(f"Iniciando scraping para URL: {url}")
     driver = initialize_driver()
-    collection, fs = connect_to_mongo("scrapping-can", "collection")
+    collection, fs = connect_to_mongo()
+
     all_scraper = ""
+    total_links_found = 0
+    total_scraped_successfully = 0
+    total_failed_scrapes = 0
+    scraped_urls = set()
+    failed_urls = set()
 
     try:
         driver.get(url)
+        
         WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.ID, "DropDownList1"))
+            EC.presence_of_element_located((By.CSS_SELECTOR, "select#DropDownList1"))
         )
 
-        dropdown = Select(driver.find_element(By.ID, "DropDownList1"))
+        dropdown = Select(driver.find_element(By.CSS_SELECTOR, "select#DropDownList1"))
+        options = dropdown.options
 
-        for index in range(len(dropdown.options)):
+        for option in options:
+            text_option = option.text.strip()
+            total_links_found += 1  
 
-            dropdown.select_by_index(index)
+            if text_option == "Abelia spathulata Siebold & Zucc.":
+                print(f"Se ingresó a: {text_option}")
+                logger.info(f"Se ingresó a: {text_option}")  
+                try:
+                    option.click()
 
-            submit_button = driver.find_element(By.XPATH, "//input[@type='submit']")
-            submit_button.click()
-            try:
-                WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.ID, "GridView1"))
-                )
+                    submit_button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='submit']"))
+                    )
+                    submit_button.click()
 
-                page_soup = BeautifulSoup(driver.page_source, "html.parser")
-                table = page_soup.find("table", {"id": "GridView1"})
-                if table:
-                    
-                    rows = table.find_all("tr")
-                    for row in rows:
-                        columns = row.find_all("td")
-                        row_data = [col.get_text(strip=True) for col in columns]
-                        all_scraper += (
-                            " ".join(row_data) + "\n"
-                        ) 
-                
-            except:
-                print(f"Error inesperado para la opción {index + 1}. Excepción: {str(e)}")
+                    WebDriverWait(driver, 40).until(
+                        EC.presence_of_element_located((By.TAG_NAME, "body"))
+                    )
 
-            driver.back()
+                    page_soup = BeautifulSoup(driver.page_source, "html.parser")
+                    table = page_soup.find("table", {"id": "GridView1"})
 
-            WebDriverWait(driver, 60).until(
-                EC.presence_of_element_located((By.ID, "DropDownList1"))
-            )
+                    if table:
+                        rows = table.find_all("tr")
+                        for row in rows:
+                            columns = row.find_all("td")
+                            row_data = [col.get_text(strip=True) for col in columns]
+                            all_scraper += f"{url} | {' '.join(row_data)}\n"
+                        total_scraped_successfully += 1
+                        scraped_urls.add(url)
+                    else:
+                        body_content = page_soup.find("body").get_text(strip=True)
+                        total_failed_scrapes += 1
+                        failed_urls.add(url)
 
-        response = process_scraper_data(all_scraper, url, sobrenombre, collection, fs)
+                except Exception as e:
+                    logger.error(f"Error al extraer datos de {url}: {str(e)}")
+                    total_failed_scrapes += 1
+                    failed_urls.add(url)
+
+                time.sleep(5)
+                break  
+
+        all_scraper += f"Total enlaces encontrados: {total_links_found}\n"
+        all_scraper += f"Total scrapeados con éxito: {total_scraped_successfully}\n"
+        all_scraper += "URLs scrapeadas:\n" + "\n".join(scraped_urls) + "\n"
+        all_scraper += f"Total fallidos: {total_failed_scrapes}\n"
+        all_scraper += "URLs fallidas:\n" + "\n".join(failed_urls) + "\n"
+
+        response = process_scraper_data_v2(all_scraper, url, sobrenombre)
         return response
 
-
     except Exception as e:
+        logger.error(f"Error general en el scraper: {str(e)}")
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     finally:
         driver.quit()
+        logger.info("Navegador cerrado.")
