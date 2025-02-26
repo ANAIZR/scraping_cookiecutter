89351bed0bdbe3516fa2e4c85237d039078693b3
qumadev <adrian.mamani.dev@gmail.com>
2025-02-26 16:20:18 -0500

    url_scraper_extento_MONGO

--- src/apps/shared/utils/scrapers/extento.py
+++ src/apps/shared/utils/scrapers/extento.py
@@ -6,17 +6,23 @@ from rest_framework import status
 import time
 from selenium.common.exceptions import  WebDriverException
 from ..functions import (
-    process_scraper_data,
+    process_scraper_data_v2,
     connect_to_mongo,
     get_logger,
     initialize_driver,
 )
+from datetime import datetime
+from bson import ObjectId
+
 def scraper_extento(url, sobrenombre):
     logger = get_logger("scraper")
     logger.info(f"Iniciando scraping para URL: {url}")
     driver = initialize_driver()
     collection, fs = connect_to_mongo("scrapping-can", "collection")
     all_scraper = ""
+    non_scraped_urls = []  
+    scraped_urls = []
+    total_scraped_links = 0
 
     try:
         driver.get(url)
@@ -71,9 +77,27 @@ def scraper_extento(url, sobrenombre):
                                     )
                                     if body:
                                         body_content = body.text
-                                        if body_content.strip():
-                                            all_scraper += body_content
-                                            all_scraper += "\n\n"
+                                        if body_content:
+                                            object_id = fs.put(
+                                                body_content.encode("utf-8"),
+                                                source_url=href,
+                                                scraping_date=datetime.now(),
+                                                Etiquetas=["planta", "plaga"],
+                                                contenido=body_content,
+                                                url=url
+                                            )
+                                            total_scraped_links += 1
+                                            scraped_urls.append(href)
+                                            logger.info(f"Archivo almacenado en MongoDB con object_id: {object_id}")
+        
+                                            existing_versions = list(fs.find({"source_url": href}).sort("scraping_date", -1))
+                                            if len(existing_versions) > 1:
+                                                oldest_version = existing_versions[-1]
+                                                file_id = oldest_version._id  # Esto obtiene el ID correcto
+                                                fs.delete(file_id)  # Eliminar la versión más antigua
+                                                logger.info(f"Se eliminó la versión más antigua con object_id: {file_id}")
+                                        else:
+                                            non_scraped_urls.append(href)            
 
                                     time.sleep(2)
                                 finally:
@@ -96,10 +120,15 @@ def scraper_extento(url, sobrenombre):
                     except WebDriverException as e:
                         print(f"Error al intentar volver a la página anterior: {e}")
 
-        response = process_scraper_data(all_scraper, url, sobrenombre, collection, fs)
-        logger.info("Scraping completado exitosamente.")
-        return response
+        all_scraper = (
+            f"Total enlaces scrapeados: {len(scraped_urls)}\n"
+            f"URLs scrapeadas:\n" + "\n".join(scraped_urls) + "\n\n"
+            f"Total enlaces no scrapeados: {len(non_scraped_urls)}\n"
+            f"URLs no scrapeadas:\n" + "\n".join(non_scraped_urls) + "\n"
+        )
 
+        response = process_scraper_data_v2(all_scraper, url, sobrenombre)
+        return response
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
