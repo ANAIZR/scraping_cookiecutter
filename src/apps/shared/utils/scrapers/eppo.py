c9c432388154373bd002da3e399d63d76b6a73ae
qumadev <adrian.mamani.dev@gmail.com>
2025-02-26 16:02:12 -0500

    url_scraper_eppo_MONGO

--- src/apps/shared/utils/scrapers/eppo.py
+++ src/apps/shared/utils/scrapers/eppo.py
@@ -6,16 +6,22 @@ from rest_framework.response import Response
 from rest_framework import status
 import time
 from ..functions import (
-    process_scraper_data,
+    process_scraper_data_v2,
     connect_to_mongo,
     get_logger,
     initialize_driver,
 )
+from datetime import datetime
+from bson import ObjectId
 
 def scraper_eppo(
     url,
     sobrenombre,
 ):
+    non_scraped_urls = []  
+    scraped_urls = []
+    total_scraped_links = 0
+
     logger = get_logger("scraper")
     logger.info(f"Iniciando scraping para URL: {url}")
     driver = initialize_driver()
@@ -60,7 +66,30 @@ def scraper_eppo(
                                 "div.row>div.col-md-12>div.row>div.col-md-9"
                             )
                             if content:
-                                all_scraper += content.get_text() + "\n"
+                                # all_scraper += content.get_text() + "\n"
+                                page_text = content.get_text()
+                                if page_text:
+                                    object_id = fs.put(
+                                        page_text.encode("utf-8"),
+                                        source_url=href,
+                                        scraping_date=datetime.now(),
+                                        Etiquetas=["planta", "plaga"],
+                                        contenido=page_text,
+                                        url=url
+                                    )
+                                    total_scraped_links += 1
+                                    scraped_urls.append(href)
+                                    logger.info(f"Archivo almacenado en MongoDB con object_id: {object_id}")
+
+                                    existing_versions = list(fs.find({"source_url": href}).sort("scraping_date", -1))
+                                    if len(existing_versions) > 1:
+                                        oldest_version = existing_versions[-1]
+                                        file_id = oldest_version._id  # Esto obtiene el ID correcto
+                                        fs.delete(file_id)  # Eliminar la versión más antigua
+                                        logger.info(f"Se eliminó la versión más antigua con object_id: {file_id}")
+                                else:
+                                    non_scraped_urls.append(href)
+
 
                             time.sleep(5)
                             driver.back()
@@ -74,8 +103,14 @@ def scraper_eppo(
                 except Exception as e:
                     print(f"Error al procesar la fila o hacer clic en el enlace: {e}")
             break
-        response = process_scraper_data(all_scraper, url, sobrenombre, collection, fs)
-        logger.info("Scraping completado exitosamente.")
+        all_scraper = (
+            f"Total enlaces scrapeados: {len(scraped_urls)}\n"
+            f"URLs scrapeadas:\n" + "\n".join(scraped_urls) + "\n\n"
+            f"Total enlaces no scrapeados: {len(non_scraped_urls)}\n"
+            f"URLs no scrapeadas:\n" + "\n".join(non_scraped_urls) + "\n"
+        )
+
+        response = process_scraper_data_v2(all_scraper, url, sobrenombre)
         return response
 
     except Exception as e:
