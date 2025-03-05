f8689d1c67a4390e77ff07c999a0fef8e44973a3
ANAIZR <ani.rojas.sen@gmail.com>
2025-03-05 15:26:52 -0500

    refactor: update router to directly use ScraperURLViewSet and remove unused imports

--- src/apps/shared/api/router.py
+++ src/apps/shared/api/router.py
@@ -2,16 +2,16 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from django.urls import path
 from .viewsets.login_viewsets import LoginView
-from .viewsets import get_viewset_scraper, scraperURL_viewsets
 from .viewsets.scraperFixture_viewsets import ScraperAPIView
 from .viewsets.scraperURL_viewsets import (
     SpeciesViewSet,
     ReportComparisonDetailView,
     SpeciesSubscriptionViewSet,
+    ScraperURLViewSet,
 )
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
-router.register(r"urls", scraperURL_viewsets.ScraperURLViewSet, basename="urls")
+router.register(r"urls", ScraperURLViewSet, basename="urls")
 router.register(r"species", SpeciesViewSet, basename="species")
 router.register(
     r"subscription", SpeciesSubscriptionViewSet, basename="subscription"
@@ -19,9 +19,6 @@ router.register(
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path(
-        "scraper-get-url/", get_viewset_scraper.get_scraper_url, name="get_scraper_url"
-    ),
     path("scraper-url/", ScraperAPIView.as_view(), name="scraper_url"),
     path(
         "report-comparison/<int:scraper_id>/",
