0713e83171f79b25b4688ba138a8ca196e44ba34
ANAIZR <ani.rojas.sen@gmail.com>
2025-03-05 16:36:14 -0500

    feat: update login URL path to include versioning

--- config/api_router.py
+++ config/api_router.py
@@ -10,7 +10,7 @@ router.register("users", UsuarioView, basename="users")
 additional_urlpatterns = [
     path('users/password-reset-request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
     path('users/password-reset/', PasswordResetView.as_view(), name='password-reset'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('v1/login/', LoginView.as_view(), name='login'),
 ]
 
 urlpatterns = additional_urlpatterns + router.urls
