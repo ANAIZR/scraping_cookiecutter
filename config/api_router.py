21187d6aeedf388a55c544d8170f1730472b0272
ANAIZR <ani.rojas.sen@gmail.com>
2025-03-05 16:23:40 -0500

    feat: add login endpoint to API router

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
-from src.apps.users.api.views import UsuarioView, PasswordResetRequestView, PasswordResetView
+from src.apps.users.api.views import UsuarioView, PasswordResetRequestView, PasswordResetView, LoginView
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
@@ -10,6 +10,7 @@ router.register("users", UsuarioView, basename="users")
 additional_urlpatterns = [
     path('users/password-reset-request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
     path('users/password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('/login', LoginView.as_view(), name='login'),
 ]
 
 urlpatterns = additional_urlpatterns + router.urls
