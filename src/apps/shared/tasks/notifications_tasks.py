e9a3ac086553cbb12930f842204c6408c7ede9d4
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-03-06 17:29:39 -0500

    refactor: agregar tarea de Celery para verificar nuevas especies y enviar notificaciones

---
+++ src/apps/shared/tasks/notifications_tasks.py
@@ -0,0 +1,10 @@
+from celery import shared_task
+from src.apps.shared.services.notifications import SpeciesNotificationService
+import logging
+
+logger = logging.getLogger(__name__)
+
+@shared_task(bind=True)
+def check_new_species_task(self, urls):
+    notification_service = SpeciesNotificationService()  
+    notification_service.check_new_species_and_notify(urls) 
