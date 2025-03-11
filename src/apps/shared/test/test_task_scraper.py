618cadb648160113c9224767d077b602d42c38f9
Anaiz Rojas <ani.rojas.sen@gmail.com>
2025-03-11 17:21:02 -0500

    fix: corregir la definición de la prueba test_scraper_url_task_success para incluir el parámetro self

--- src/apps/shared/test/test_task_scraper.py
+++ src/apps/shared/test/test_task_scraper.py
@@ -49,7 +49,7 @@ class TestScraperTasks:
 
     @patch("src.apps.shared.tasks.scraper_tasks.WebScraperService") 
     @patch("src.apps.shared.tasks.scraper_tasks.ScraperURL") 
-    def test_scraper_url_task_success(mock_scraper_url_model, mock_scraper_service): 
+    def test_scraper_url_task_success(self,mock_scraper_url_model, mock_scraper_service): 
         mock_scraper_url_model.objects.filter.return_value.exists.return_value = False
 
         mock_scraper_url = MagicMock(sobrenombre="test", estado_scrapeo="pendiente")
