29101daa7432b8dc07250b347411853ccaf86ac2
ANAIZR <ani.rojas.sen@gmail.com>
2025-03-21 11:32:19 -0500

    refactor: remove unused imports in species_viewsets

--- src/apps/shared/api/viewsets/species_viewsets.py
+++ src/apps/shared/api/viewsets/species_viewsets.py
@@ -19,7 +19,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from ..filter import CabiSpeciesFilter, ScraperURLFilter
 from rest_framework import generics
 from rest_framework.response import Response
-from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
@@ -36,7 +35,6 @@ from django.conf import settings
 from src.apps.users.services.email import EmailService
 from rest_framework.pagination import PageNumberPagination
 from src.apps.shared.services.resume_service import ResumeService
-from rest_framework.request import Request  
 
 logger = logging.getLogger(__name__)
 
